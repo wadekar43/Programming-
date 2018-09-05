64bd75132d7e4fbb24724dddc8347df6f909620a
Saurabh R Wadekar <saurabhwadekar14@gmail.com>
2018-09-06 00:42:06 +0530

    Play rock,paper & scissor with PC

---
+++ RSP.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+from random import randint
+player = input('rock(r), paper(p) or scissor(s)?')
+print(player, 'vs', end=' ')
+
+chosen = randint(1,3)
+#print(chosen)
+
+if chosen == 1:
+  computer = 'r'
+  
+elif chosen == 2:
+  computer = 'p'
+    
+else:
+  computer = 's'
+
+print(computer)
+
+if player == computer:
+  print("DRAW")
+elif (player == 'r' and computer == 's'):
+  print("Player Wins")
+elif(player == 'r' and computer == 'p'):
+  print("Computer Wins")
+elif(player == 'p' and computer == 'r'):
+  print("Player Wins")
+elif(player == 'p' and computer == 's'):
+  print("Computer Wins")
+elif(player == 's' and computer == 'p'):
+  print("Player Wins")  
+elif(player == 's' and computer == 'r'):
+  print("Computer Wins")
